b6d97d4f438c5e55b7f51958d51292fed41c7f74
James Mosquera <Jamesmosqr@gmail.com>
2026-03-09 16:17:40 -0500

    Trust all proxies for HTTPS behind Railway load balancer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         App\Providers\TenancyServiceProvider::class,
     ])
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->redirectGuestsTo(function (\Illuminate\Http\Request $request) {
             if (tenancy()->initialized) {
                 return '/' . tenant('id') . '/admin/login';
